d2e5fb1b180266833c198e0304be6f68b0254724
Connor Johnson <connor.labaume.johnson@gmail.com>
2024-05-12 14:56:37 -0600

    Markup up main view with sections

--- backend/chart/views.py
+++ backend/chart/views.py
@@ -3,6 +3,8 @@ from turbo_helper import turbo_stream
 
 import random
 
+# ----- General Functions ----- #
+
 N = 40
 w = 0.95
 
@@ -23,8 +25,12 @@ def bundle_data_for_chartjs(N: int, w: float) -> dict[str, list]:
     }
     return data
 
+# ----- Home View ----- #
+
 def home(request):
     return render(request, 'home.html')
+
+# ----- Turbo Drive Functionality ----- #
 
 def draw_line_chart(request):
     '''Draw a line chart with Chart.js.
@@ -42,6 +48,8 @@ def draw_histogram(request):
     data = bundle_data_for_chartjs(N, w)
     return render(request, 'histogram.html', data)
 
+# ----- Turbo Frame Functionality ----- #
+
 def draw_inline_line_chart(request):
     '''Draw an inline line chart with Chart.js.
 
@@ -50,8 +58,12 @@ def draw_inline_line_chart(request):
     data = bundle_data_for_chartjs(N, w)
     return render(request, 'inline-line-chart.html', data)
 
+# ----- Turbo Stream Functionality ----- #
+
 def helper_stream(request):
-    '''Used for django-turbo-helper example.
+    '''Used for django-turbo-helper endpoint.
+
+    Used for Turbo Stream demo.
     '''
     data = bundle_data_for_chartjs(N, w)
     response = turbo_stream.response([
